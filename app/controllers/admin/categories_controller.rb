94d4be785d3373772f2f20b55825b6e251f656c0
tw5033 <tw5033@gmail.com>
2019-05-15 20:57:04 +0000

    Finish admin add categories to db

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,20 +2,27 @@ class Admin::CategoriesController < ApplicationController
 
   def index
     @categories = Category.order(id: :desc).all
-    # @products = Product.order(id: :desc).all
   end
 
   def new
-    # @product = Product.new
+    @category = Category.new
   end
 
   def create
-    # @product = Product.new(product_params)
+    @category = Category.new(category_params)
 
-    # if @product.save
-    #   redirect_to [:admin, :products], notice: 'Product created!'
-    # else
-    #   render :new
-    # end
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+  
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
   end
 end
