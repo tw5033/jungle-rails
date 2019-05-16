a6c98ed8fe72b503262c55617f03a70ce1ed857c
tw5033 <tw5033@gmail.com>
2019-05-16 23:53:58 +0000

    Generate mailer controller/views and implement email functionality

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,10 @@
+class UserMailer < ApplicationMailer
+  default from: 'no-reply@jungle.com'
+
+  def order_email(order)
+    @order = order
+    @url = 'http://localhost:3000/'
+    mail(to: order.email, subject: "Jungle Order Confirmation: Order ##{order.id}")
+  end
+
+end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,7 @@
 class Order < ActiveRecord::Base
 
+  after_create :send_email
+
   belongs_to :user
   has_many :line_items
 
@@ -7,4 +9,9 @@ class Order < ActiveRecord::Base
 
   validates :stripe_charge_id, presence: true
 
+  private
+
+  def send_email
+    UserMailer.order_email(self).deliver
+  end
 end
