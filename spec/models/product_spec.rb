73ffb7566df0f2475644b1208b10c5c61e299a0a
tw5033 <tw5033@hotmail.com>
2019-05-20 12:59:54 -0700

    Finish writing validation checks for product model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,67 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    category = Category.new
+
+    it 'is valid with all fields' do
+      product = Product.create(
+        name: 'test',
+        description: 'test',
+        image: 'test',
+        price_cents: 10,
+        quantity: 1,
+        category: category
+      )
+      expect(product).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      product = Product.create(
+        name: nil,
+        description: 'test',
+        image: 'test',
+        price_cents: 10,
+        quantity: 1,
+        category: category
+      )
+      expect(product.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it 'is not valid without a price' do
+      product = Product.create(
+        name: 'test',
+        description: 'test',
+        image: 'test',
+        price_cents: nil,
+        quantity: 1,
+        category: category
+      )
+      expect(product.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it 'is not valid without a quantity' do
+      product = Product.create(
+        name: 'test',
+        description: 'test',
+        image: 'test',
+        price_cents: 10,
+        quantity: nil,
+        category: category
+      )
+      expect(product.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+    it 'is not valid without a category' do
+      product = Product.create(
+        name: 'test',
+        description: 'test',
+        image: 'test',
+        price_cents: 10,
+        quantity: 1,
+        category: nil
+      )
+      expect(product.errors.full_messages).to include("Category can't be blank")
+    end
+  end
 end
